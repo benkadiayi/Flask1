622b6b76b3d1dbd94e3f60c428c793c6f6344360
benjamin <Benkadiayi@gmail.com>
2021-10-20 18:48:43 -0500

    commit

--- car_inventory/site/routes.py
+++ car_inventory/site/routes.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from flask_login.utils import login_required
+
 
 
 site = Blueprint('site', __name__, template_folder='sites_templates')
@@ -7,10 +9,7 @@ site = Blueprint('site', __name__, template_folder='sites_templates')
 def home():
     return render_template('index.html')
 
-@site.route('/prices')
-def prices():
-    return render_template('prices.html')
-
-@site.route('/about')
-def about():
-    return render_template('about.html')
+@site.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html')
--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class config():
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Excellence@127.0.0.1:5432/car-collection' or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
